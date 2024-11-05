cd3b0e3ae85b90a7a951d99d27bc221b4e60e008
beva3 <mizabienvenu@gmail.com>
2024-11-05 11:46:26 +0530

    project doe

--- calculator.c
+++ calculator.c
@@ -2,6 +2,33 @@
 
 void run_calculator(){
     printf("run calculator ...\n");
+    double n1 = 0, n2 = 0, return_value = 0;
+    char operator = ' ';
+
+    while(1){
+        printf("Enter a number (or <q> to exit) : ");
+        if(scanf("%lf", &n1) != 1){
+            break;// exit if the input_user is 'q
+        }
+        printf("Enter an operator (+,-,*,/,^,r) : ");
+        if(scanf(" %c", &operator) != 1){
+            handle_error("Invalid operator! ");
+            continue;
+        }else{
+            if(operator == 'r'){
+                return_value = square_root(n1);
+                printf("the square root of %.2f is %.2f\n",n1,return_value);
+            }else{
+                printf("Enter another number : ");
+                if(scanf("%lf", &n2) != 1){
+                    handle_error("Invalid number! ");
+                    continue;
+                }
+                return_value = perform_operation(n1, n2, operator);
+                printf("Result : %.2f\n", return_value);
+            }
+        }
+    }
 }
 
 void handle_error(const char *message){
